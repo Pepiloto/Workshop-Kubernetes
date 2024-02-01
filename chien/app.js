93718a6a02508562cc92b717f4ad2b140c15cd5e
Killian Fleury <killian.fleury@epitech.eu>
2024-02-01 17:50:12 +0100

    fix ctrl X

--- chien/app.js
+++ chien/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Vous pouvez choisir le port que vous préférez
+const port = 3000;
 
 // Définir une route pour la méthode GET
 app.get('/', (req, res) => {
@@ -11,7 +11,24 @@ app.get('/chien', (req, res) => {
     res.send('chat');
 });
 
+// Intercepter le signal SIGTERM
+process.on('SIGTERM', () => {
+    console.info('Received SIGTERM signal. Cleaning up before exit.');
+    // Ajoutez ici les opérations de nettoyage ou d'arrêt de l'application si nécessaire
+    process.exit(0); // Terminate the process gracefully
+});
+
 // Démarrer le serveur
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
+
+// Intercepter le signal SIGINT (équivalent à Ctrl+C dans le terminal)
+process.on('SIGINT', () => {
+    console.info('Received SIGINT signal. Cleaning up before exit.');
+    // Ajoutez ici les opérations de nettoyage ou d'arrêt de l'application si nécessaire
+    server.close(() => {
+        console.log('Server closed. Exiting.');
+        process.exit(0);
+    });
 });
