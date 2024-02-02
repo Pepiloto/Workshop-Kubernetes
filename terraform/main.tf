5e6673dfe70399bfd59aad41c991d2f17e6e8702
Pepiloto <pepiloto@pepiloto.dev>
2024-02-02 16:41:09 +0100

    Ouais

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,47 @@ provider "kubernetes" {
   config_context = "kind-kind"
 }
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "namespace" {
   metadata {
     name = "app"
   }
 }
 
+resource "kubernetes_deployment" "deployment" {
+  metadata {
+    name = "myapp"
+    namespace = "app"
+  }
+  spec {
+    replicas = 3
+    selector {
+      match_labels = {
+        app = "myapp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "myapp"
+        }
+      }
+      spec {
+        container {
+          image = "app:latest"
+          image_pull_policy = "IfNotPresent"
+          name  = "myapp"
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
